3c7be38ec4c7d1395d392f23a0d4c6c345905dc8
Alan Christopher Thomas <alan@alanct.com>
2014-04-27 14:21:41 -0500

    better colors

--- .bash_scripts/pwd-prompt.bash
+++ .bash_scripts/pwd-prompt.bash
@@ -2,9 +2,10 @@
 # http://alanct.com
 
 # Readable variables
-Yellow="\[\033[0;33m\]"
-White="\[\033[0;37m\]"
-IBlack="\[\033[0;90m\]"
+Yellow="\[\033[38;5;227m\]"
+Cyan="\[\033[38;5;152m\]"
+Orange="\[\033[38;5;202m\]"
+Gray="\[\033[38;5;240m\]"
 ColorOff="\[\033[0m\]"
 Hostname="\h"
 
@@ -12,13 +13,13 @@ Hostname="\h"
 export PROMPT_COMMAND='CurDir=`echo ${PWD/#$HOME/\~}|sed -re "s!([^/.|^/])[^/]+/!\1/!g"`'
 
 # Export the prompt
-export PS1=$IBlack$Hostname$Color_Off'$(git branch &>/dev/null;\
+export PS1=$Gray$Hostname$Color_Off'$(git branch &>/dev/null;\
 if [ $? -eq 0 ]; then \
     echo "$(echo `git status` | grep "nothing to commit" > /dev/null 2>&1; \
     if [ "$?" -eq "0" ]; then \
-        echo "'$White'"$(__git_ps1 " [%s]"); \
+        echo "'$Cyan'"$(__git_ps1 " [%s]"); \
     else \
-        echo "'$White'"$(__git_ps1 " {%s}"); \
+        echo "'$Orange'"$(__git_ps1 " {%s}"); \
     fi) '$Yellow'$CurDir'$ColorOff'\$ "; \
 else \
     echo " '$Yellow'$CurDir'$ColorOff'\$ "; \
